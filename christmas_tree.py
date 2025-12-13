3deb329ae140ab4b188221485e3a89f3ca1846ad
pesman129 <163185251+pesman129@users.noreply.github.com>
2025-12-13 18:02:11 +0100

    Add files via upload

---
+++ christmas_tree.py
@@ -0,0 +1,25 @@
+from random import randint
+from os import system
+from time import sleep
+from termcolor import colored
+import tree_generator
+
+tree = tree_generator.main(int(input("What is the height of the tree: ")))
+
+colors = {1: "red", 2: "blue", 3: "yellow"}
+
+while True:
+	for i in tree:
+		if i == "*":
+			cube = randint(1, 3)
+			if cube == 1:
+				print(colored('*', colors[randint(1, 3)]), end="")
+			else:
+				print(colored("*", "green"), end="")
+		elif i == "|":
+			print(i)
+			sleep(0.5)
+			system("clear")
+		else:
+			print(i, end="")
+
