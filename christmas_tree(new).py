7cdf463a08a2dc524db026b306aa8a3001a3d337
pesman129 <163185251+pesman129@users.noreply.github.com>
2025-12-17 18:23:53 +0100

    Update christmas_tree(new).py

--- christmas_tree(new).py
+++ christmas_tree(new).py
@@ -3,10 +3,10 @@ from os import system
 from time import sleep
 from termcolor import colored
 import tree_generator
-from sys import argv
+from sys import argv, stdout
 import platform
 
-clear = {"linux": "clear", "macos": "clear", "windows": "cls"}
+term = stdout.isatty()
 tree = tree_generator.main(int(input("What is the height of the tree: ")))
 colors = {1: "red", 2: "blue", 3: "yellow"}
 leaf_color = "green"
@@ -26,7 +26,9 @@ while True:
 		elif i == "|":
 			print(i)
 			sleep(0.5)
-			system(clear[platform.system().lower()])
+			if term == True:
+				print("\033[2J\033[H", end="")
+			else:
+				print("\n" * 100)
 		else:
 			print(i, end="")
-
